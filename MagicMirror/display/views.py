4f1956d2e6e663ee55429c9f14c8bb2816d98c77
Unknown <hu_tianyi@outlook.com>
2018-03-21 20:57:27 +0800

    搭建后台业务

--- MagicMirror/display/views.py
+++ MagicMirror/display/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.html import HttpResponse
+from models import Devices, Tips
+import json
 
 # Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def devices(request):
+    devices_list = Devices.objects.all().order_by('location')
+    return HttpResponse(json.dumps(devices_list),content_type='applicatiion/json')
+    
