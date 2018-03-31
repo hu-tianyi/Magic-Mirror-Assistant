decf873b6a8f6bfa9d7921eb1b472075dc32f98e
Unknown <hu_tianyi@outlook.com>
2018-03-31 17:47:32 +0800

    语音控制插件 中文：坑

--- contrib/Connect.py
+++ contrib/Connect.py
@@ -14,28 +14,28 @@ def handle(text, mic, profile, wxbot=None):
     logger = logging.getLogger(__name__)
     try:
         if any(word in text for word in [u"开启", u"打开"]):
-            if any(word in text for word in [u"一号", u"1号"])：
+            if any(word in text for word in [u"一号", u"1号"]):
                 #发送一号打开命令
                 print('发送一号打开命令')
                 mic.say('正在开启一号插座', cache=True)
-            if any(word in text for word in [u"二号", u"2号"])：
+            if any(word in text for word in [u"二号", u"2号"]):
                 #发送二号打开命令
                 print('发送二号打开命令')
                 mic.say('正在开启二号插座', cache=True)
-            if any(word in text for word in [u"三号", u"3号"])：
+            if any(word in text for word in [u"三号", u"3号"]):
                 #发送三号打开命令
                 print('发送三号打开命令')
                 mic.say('正在开启三号插座', cache=True)
         elif any(word in text for word in [u"关闭", u"关机"]):
-            if any(word in text for word in [u"一号", u"1号"])：
+            if any(word in text for word in [u"一号", u"1号"]):
                 #发送一号关闭命令
                 print('发送一号关闭命令')
                 mic.say('正在关闭一号插座', cache=True)
-            if any(word in text for word in [u"二号", u"2号"])：
+            if any(word in text for word in [u"二号", u"2号"]):
                 #发送二号关闭命令
                 print('发送二号关闭命令')
                 mic.say('正在关闭二号插座', cache=True)
-            if any(word in text for word in [u"三号", u"3号"])：
+            if any(word in text for word in [u"三号", u"3号"]):
                 #发送三号关闭命令
                 print('发送三号关闭命令')
                 mic.say('正在关闭三号插座', cache=True)
@@ -43,6 +43,5 @@ def handle(text, mic, profile, wxbot=None):
         logger.error(e)
         mic.say('抱歉，物联网控制插件出错', cache=True)
 
-                                                            
 def isValid(text):
     return any(word in text for word in [u"插座", u"物联网", u"排插"])
